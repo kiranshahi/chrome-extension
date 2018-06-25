8649a6c353c6b62b6319a925d065ee14d37794ed
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-06-25 16:08:09 +0545

    extension added.

---
+++ switch.js
@@ -0,0 +1,3 @@
+for (var i = 0; document.images.length; i++) {
+    document.images[i].src = "https://camo.githubusercontent.com/e5b1d847f420c7c4e9a2e0c7e2e5c2f6cc078e6c/68747470733a2f2f692e696d6775722e636f6d2f6d52386d7562672e6a7067"
+}
